2348c775db839ed90eb805e4e2d75e4e58ed0e25
Osman Fikret Ceylan <ofcyln@gmail.com>
2019-03-22 14:59:41 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [ './app.component.scss' ]
 })
-export class AppComponent {
-  title = 'ng-pwa';
+export class AppComponent implements OnInit {
+  isBannerShown: boolean;
+
+  constructor(private toasterService: ToastrService) {
+  }
+
+  ngOnInit(): void {
+    setTimeout(
+      () => this.toasterService.success('Op 22 Maart hebben we de FrontMen PWA-App gemaakt.', 'Google Day'),
+      100
+    );
+  }
+
+  showIosInstallBanner(): void {
+    const isIos = () => {
+      const userAgent = window.navigator.userAgent.toLowerCase();
+      return /iphone|ipad|ipod/.test(userAgent);
+    };
+
+    const isInStandaloneMode = () => ( 'standalone' in window.navigator )
+      && window.matchMedia('(display-mode: standalone)').matches;
+
+    const isBannerShown = this.isBannerShown;
+
+    if (isIos() && !isInStandaloneMode() && isBannerShown === undefined) {
+      this.isBannerShown = true;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { SplashScreenComponent } from './components/splash-screen/splash-screen.component';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SplashScreenComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
